9aa9bd6ae5d3cd075c43a132368ed3bf6dd8d9d8
zycoJamie <410752387@qq.com>
2019-01-16 01:13:10 +0800

    optimize: recyclerview pull and back scan layout

--- app/src/main/java/com/jamie/zyco/pdfer/widget/MyRecyclerView.kt
+++ app/src/main/java/com/jamie/zyco/pdfer/widget/MyRecyclerView.kt
@@ -16,11 +16,12 @@ class MyRecyclerView : RecyclerView {
         val TAG = MyRecyclerView::class.java.simpleName
     }
 
-    private val mFriction = 2.4f  //阻力系数
+    private val mFriction = 3f  //阻力系数
     private var initPointY: Float = 0f
-    private var mHeaderHight = 0
-    private val mThreshold = 300  //header最大高度
+    private var mHeaderHeight = 0
+    private val mThreshold = 400  //header最大高度
     private val mAutoScrollThreshold = 150 //触发header自动滑动的最大高度
+    private var isExtension = false
     private val mScroller: Scroller by lazy {
         Scroller(context)
     }
@@ -40,27 +41,38 @@ class MyRecyclerView : RecyclerView {
         when (e!!.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
                 initPointY = e.rawY
+                if (!mScroller.isFinished) {
+                    mScroller.forceFinished(true) //当scroller到达final坐标后，并未立即结束
+                }
             }
             MotionEvent.ACTION_MOVE -> {
                 val layoutManager = layoutManager as LinearLayoutManager
                 val headerWrapperAdapter = adapter as HeaderWrapperAdapter
                 if (scrollState == RecyclerView.SCROLL_STATE_DRAGGING
                         && layoutManager.findFirstCompletelyVisibleItemPosition() == 0 && e.rawY - initPointY > 0
-                        && mHeaderHight <= mThreshold
-                        && headerWrapperAdapter.getHeaderCount() > 0) {
+                        && mHeaderHeight <= mThreshold
+                        && headerWrapperAdapter.getHeaderCount() > 0
+                        && !isExtension) {
+                    Zog.log(0, TAG, "pull")
                     move(e)
                 }
-                if (mHeaderHight == mThreshold && e.rawY - initPointY < 0) {
+                if (isExtension && e.rawY - initPointY < 0) {
+                    Zog.log(0, TAG, "back")
                     move(e)
                 }
             }
             MotionEvent.ACTION_UP -> {
-                if (mHeaderHight in mAutoScrollThreshold until mThreshold) {
-                    mScroller.startScroll(0, mHeaderHight, 0, mThreshold - mHeaderHight, 500)
+                isExtension = mHeaderHeight >= mAutoScrollThreshold
+                if (mHeaderHeight in mAutoScrollThreshold until mThreshold) {
+                    Zog.log(0, TAG, "start auto pull $mHeaderHeight ****")
+                    mScroller.startScroll(0, mHeaderHeight, 0, mThreshold - mHeaderHeight, 500)
+                    Zog.log(0, TAG, "finalY ${mScroller.finalY}")
                     invalidate()
                 }
-                if (mHeaderHight in 1 until mAutoScrollThreshold) {
-                    mScroller.startScroll(0, mHeaderHight, 0, -mHeaderHight, 500)
+                if (mHeaderHeight in 1 until mAutoScrollThreshold) {
+                    Zog.log(0, TAG, "start auto back $mHeaderHeight ****")
+                    mScroller.startScroll(0, mHeaderHeight, 0, -mHeaderHeight, 500)
+                    Zog.log(0, TAG, "finalY ${mScroller.finalY}")
                     invalidate()
                 }
             }
@@ -70,31 +82,38 @@ class MyRecyclerView : RecyclerView {
 
     private fun move(e: MotionEvent) {
         val headerWrapperAdapter = adapter as HeaderWrapperAdapter
-        mHeaderHight = Math.abs((e.rawY - initPointY) / mFriction).toInt()
-        Zog.log(0, TAG, "header hight:$mHeaderHight")
+        mHeaderHeight = if (!isExtension) {
+            ((e.rawY - initPointY) / mFriction).toInt()
+        } else {
+            ((mThreshold - (initPointY - e.rawY))).toInt()
+        }
+        Zog.log(0, TAG, "header height:$mHeaderHeight before")
         val header: View? = headerWrapperAdapter.getHeaderItem(0)
         val layoutParams = header?.layoutParams
-        if (mHeaderHight > mThreshold) {
-            mHeaderHight = mThreshold
+        if (mHeaderHeight >= mThreshold) {
+            mHeaderHeight = mThreshold
         }
-        if (mHeaderHight < 0) {
-            mHeaderHight = 0
+        if (mHeaderHeight < 0) {
+            mHeaderHeight = 0
         }
-        layoutParams?.height = mHeaderHight
+        Zog.log(0, TAG, "header height:$mHeaderHeight after")
+        layoutParams?.height = mHeaderHeight
         header?.layoutParams = layoutParams
         smoothScrollToPosition(0)
     }
 
     override fun computeScroll() {
         if (mScroller.computeScrollOffset()) {
-            Zog.log(0, TAG, "computeScroll")
+            Zog.log(0, TAG, "computeScroll ${mScroller.isFinished}")
             val headerWrapperAdapter = adapter as HeaderWrapperAdapter
             val header: View? = headerWrapperAdapter.getHeaderItem(0)
             val layoutParams = header?.layoutParams
+            val lastY = layoutParams?.height
             layoutParams?.height = mScroller.currY
+            mHeaderHeight = mScroller.currY
             header?.layoutParams = layoutParams
-            Zog.log(0, TAG, "${mScroller.currY}")
-            smoothScrollBy(0, mScroller.currY - scrollY)
+            Zog.log(0, TAG, "${mScroller.currY}-$lastY")
+            smoothScrollBy(0, mScroller.currY - lastY!!)
             postInvalidate()
         }
     }
